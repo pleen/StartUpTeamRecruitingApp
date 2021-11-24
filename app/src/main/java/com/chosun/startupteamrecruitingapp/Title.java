53bd4c45b8c915e3b664b33bb1f95b6cfeeb7e4e
pleen12 <pleen12@naver.com>
2021-11-24 16:38:15 +0900

    update_design

--- app/src/main/java/com/chosun/startupteamrecruitingapp/Title.java
+++ app/src/main/java/com/chosun/startupteamrecruitingapp/Title.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -18,7 +19,7 @@ public class Title extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_title, container, false);
         Button goToSignUpBtn = rootView.findViewById(R.id.goto_signup_btn);
-        Button goToSignInBtn = rootView.findViewById(R.id.goto_signin_btn);
+        ConstraintLayout goToSignInBtn = rootView.findViewById(R.id.goto_signin_btn);
         goToSignUpBtn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_title_to_signup, null));
         goToSignInBtn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_title_to_signin, null));
         return rootView;
